4d73a1c1bac311c90385b52decd00ffe96bea21d
Anatol <148008027+MagicevAnatol@users.noreply.github.com>
2024-04-26 14:29:30 +0300

    add app

---
+++ handlers/__init__.py
@@ -0,0 +1,6 @@
+from .default_handlers import start
+from .default_handlers import help
+from .custom_handlers import input_data
+from . import callback_handlers
+from .custom_handlers import history
+from .default_handlers import echo
---
+++ utils/__init__.py
@@ -0,0 +1,4 @@
+from . import misc
+from . import print_data
+from . import find_hotels
+from . import set_bot_commands
